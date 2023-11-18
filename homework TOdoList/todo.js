3fc4a81d08c381c156176f1d99b2650a98c82fbd
RomanBilbas <rebenak006@gmail.com>
2023-11-18 12:49:59 +0200

    Add js

---
+++ homework TOdoList/todo.js
@@ -0,0 +1,33 @@
+"use strict";
+const nameInput = document.querySelector(".inputName");
+nameInput.oninput = nameInputHandler;
+function nameInputHandler(e) {
+  if (/^[A-Z][a-z](1,19)$/.test(e.target.value)) {
+    e.target.classList.add(".valid");
+    e.target.classList.remove(".invalid");
+  } else {
+    e.target.classList.add(".invalid");
+    e.target.classList.remove(".valid");
+  }
+}
+
+const INPUTS_REG_EXP = {
+  "user-name": /^[A-Z][a-z]{2,19}$/,
+  "sur-name": /^[A-Z][a-z]{2,19}$/,
+  "user-email": /^.+@.+$/,
+  "user-tel": /^\d{3}-\d{3}-\d{4}$/,
+};
+
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach((i) => i.addEventListener("input", inputHandler));
+
+function inputHandler(e) {
+  if (INPUTS_REG_EXP[e.target.name].test(e.target.value)) {
+    e.target.classList.add("valid");
+    e.target.classList.remove("invalid");
+  } else {
+    e.target.classList.remove("valid");
+    e.target.classList.add("invalid");
+  }
+}
